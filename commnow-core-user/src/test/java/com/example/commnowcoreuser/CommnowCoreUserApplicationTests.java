85413ff16be34050a60a495f55d7c60038f1e81a
zezhe <zezheng.liao@commnow.cn>
2019-04-03 16:32:05 +0800

    第一次提交

---
+++ commnow-core-user/src/test/java/com/example/commnowcoreuser/CommnowCoreUserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.commnowcoreuser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommnowCoreUserApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
